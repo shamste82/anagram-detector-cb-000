68e0da8b58dc9ee325f2563d440621263f4aa7c1
Steffen Hammelow-Berg <shammelow@gmail.com>
2018-02-04 11:07:38 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,25 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(w)
+    # Sorts the words alphabetically
+    sorted_word = @word.chars.sort.join
+    sorted_w = w.map { |w| w.chars.sort.join}
+    index = Array.new
+    list_of_matches = Array.new
+    #Matches to check if anagrams
+    sorted_w.each_with_index do |x, i|
+      if x == sorted_word
+        index << i
+      end
+    end
+    #Create an array of unsorted/original matches and returns it
+    index.each { |x| list_of_matches << w[x]}
+    list_of_matches
+  end
+
+end
