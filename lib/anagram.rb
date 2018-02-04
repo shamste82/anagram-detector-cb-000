3366c27e520437b556f1057d81ef6ac8ef8abde6
Steffen Hammelow-Berg <shammelow@gmail.com>
2018-02-04 10:44:12 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,23 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(w)
+    puts "#{@word}"
+    puts "#{w}"
+    sorted_word = @word.chars.sort.join
+    sorted_w = w.map { |w| w.chars.sort.join}
+    puts("#{sorted_w}")
+    match = false
+    sorted_w.each do |w|
+      if w == sorted_wowrd
+        match = true
+      end
+    match
+
+  end
+
+end
